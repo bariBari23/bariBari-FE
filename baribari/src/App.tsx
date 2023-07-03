a9641a230a0124a2fa8934abefb44f996ea475be
hmuri <113418916+hmuri@users.noreply.github.com>
2023-07-03 21:03:55 +0900

    style: 미디어 쿼리 적용 test

--- baribari/src/App.tsx
+++ baribari/src/App.tsx
@@ -1,13 +1,32 @@
 import './App.css';
+import { useEffect } from 'react';
+import { useMediaQuery } from "react-responsive";
 import { GlobalStyle } from './styles/GlobalStyle';
+import styled from 'styled-components';
 
 
-export default function App() {
+export default function App({}) {
+    function setScreenSize(){
+        let vh = window.innerHeight*0.01;
+        document.documentElement.style.setProperty("--vh", `${vh}px`);
+    }
+    useEffect(() => {
+        setScreenSize();
+    });
     return (
         <>
             <GlobalStyle />
-            <>바리바리가 최고</>
+            <Container>바리바리가 최고</Container>
             <button>얍</button>
         </>
     );
 }
+
+
+const Container = styled.div`
+background-color: black;
+
+@media ${props => props.theme.tablet}{
+    background-color: orange;
+}
+`
