e49575e3b567d67ca15398d8f92291baf77e51a8
hmuri <113418916+hmuri@users.noreply.github.com>
2023-07-03 23:06:52 +0900

    feat: page route 및 기본 세팅

--- baribari/src/App.tsx
+++ baribari/src/App.tsx
@@ -3,9 +3,17 @@ import { useEffect } from 'react';
 import { useMediaQuery } from "react-responsive";
 import { GlobalStyle } from './styles/GlobalStyle';
 import styled from 'styled-components';
+import {Route, Routes} from 'react-router-dom'
+import Home from './page/Home';
+import Join from './page/Join';
+import Login from './page/Login';
+import Order from './page/Order';
+import OrderList from './page/OrderList';
+import Search from './page/Search';
+import StoreDetail from './page/StoreDetail';
 
 
-export default function App({}) {
+export default function App() {
     function setScreenSize(){
         let vh = window.innerHeight*0.01;
         document.documentElement.style.setProperty("--vh", `${vh}px`);
@@ -16,16 +24,34 @@ export default function App({}) {
     return (
         <Container>
             <GlobalStyle />
+                <InsideContainer>
+                    <Routes>
+                    <Route path="/" element={<Home />}/>
+                    <Route path="/join" element={<Join />}/>
+                    <Route path="/login" element={<Login />}/>
+                    <Route path="/order" element={<Order />}/>
+                    <Route path="/orderlist" element={<OrderList />}/>
+                    <Route path="/search" element={<Search />}/>
+                    <Route path="/detail" element={<StoreDetail />}/>
+                    </Routes>
+                </InsideContainer>
         </Container>
     );
 }
 
 
 const Container = styled.div`
+    display: flex;
     height: calc(var(--vh, 1vh) * 100);
-    background-color: black;
-
+    align-items: center;
+    justify-content: center;
 @media ${props => props.theme.tablet}{
     
 }
+`
+
+const InsideContainer = styled.div`
+    display: flex;
+    width: calc(100vw - 32px);
+    height: 100vh;
 `
