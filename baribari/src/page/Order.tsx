dcbb03d487adc5dd4bff77a7ffb83258457dc504
hmuri <113418916+hmuri@users.noreply.github.com>
2023-07-04 09:13:24 +0900

    feat: order페이지 시작

--- baribari/src/page/Order.tsx
+++ baribari/src/page/Order.tsx
@@ -14,19 +14,15 @@ export default function Order(){
         <Container>
             <TopBar page = {'결제하기'}/>
             <InsideBox>
-                <UserInfoBox>
+                <InfoBox>
                     <div style={{fontSize: '18px', fontWeight: '700'}}>주문자 정보</div>
                     <PhoneInput placeholder='휴대폰 번호를 입력해주세요'></PhoneInput>
-
-                </UserInfoBox>
-                <FoodImgBox/>
-                <DetailNav>
-                    <InformBtn isSelected = {active==="반찬 상세"} onClick={() => changeDetailBox('반찬 상세')}>반찬 상세</InformBtn>
-                    <InformBtn isSelected = {active==="가게 정보"} onClick={() => changeDetailBox('가게 정보')}>가게 정보</InformBtn>
-                    <InformBtn isSelected = {active==="리뷰"} onClick={() => changeDetailBox('리뷰')}>리뷰</InformBtn>
-                </DetailNav>
-                <FoodDetailBox isSelected = {active==="반찬 상세"}/>
-                <StoreDetailBox isSelected = {active==="가게 정보"}/>
+                </InfoBox>
+                <InfoBox style={{height: '201px', paddingTop: '20px'}}>
+                    <div style={{fontSize: '18px', fontWeight: '700'}}>픽업 시간</div>
+                    <PhoneInput placeholder='휴대폰 번호를 입력해주세요'></PhoneInput>
+                </InfoBox>
+            
                 <ReviewBox isSelected = {active==="리뷰"}/>
                 <AddBtn>장바구니에 넣기</AddBtn>
             </InsideBox>
@@ -48,7 +44,7 @@ const InsideBox = styled.div`
     justify-content: flex-start;
     background-color: #F9F9F9;
 `
-const UserInfoBox = styled.div`
+const InfoBox = styled.div`
     display: flex;
     height: 92px;
     background-color: white;
@@ -60,38 +56,12 @@ const PhoneInput = styled.input`
     display: flex;
     height: 44px;
     margin-top: 20px;
-    width: 100%;
     border: solid 1px #767676;
     border-radius: 8px;
     padding-left: 8px;
     color: #767676;
     font-size: 16px;
     font-weight: 600;
-`
-const FoodImgBox = styled.div`
-    display: flex;
-    width: 100%;
-    height: 240px;
-    background-color: #EFEFEF;
-`
-const DetailNav = styled.div`
-    display: flex;
-    width: 100%;
-    height: 48px;
-    background-color: #F9F9F9;
-    justify-content: space-between;
-`
-const InformBtn = styled.button<{isSelected: boolean}>`
-    width: 33.33%;
-    background: none;
-    display: flex;
-    border: none;
-    align-items: center;
-    justify-content: center;
-    color: ${(props) => (props.isSelected === true ? '#FF7455' : '#AAAAAA')};
-    font-size: 16px;
-    font-weight: ${(props) => (props.isSelected === true ? '700' : '500')};
-    border-bottom: ${(props) => (props.isSelected === true ? 'solid 2px #FF7455' : 'none')};
 `
 const AddBtn = styled.div`
     display: flex;
