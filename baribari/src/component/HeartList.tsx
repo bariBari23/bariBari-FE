b29978807fc344d3a34be7882da2939bc1ecbc6b
Yelynn Oh <yoyelynn@gmail.com>
2023-07-04 23:05:23 +0900

    style: HeartList 버튼 on/off 스타일 수정

--- baribari/src/component/HeartList.tsx
+++ baribari/src/component/HeartList.tsx
@@ -1,17 +1,28 @@
 import { styled } from 'styled-components';
+import { useState } from 'react';
 
 export default function HeartList() {
-    return <HeartListButton>찜한 가게</HeartListButton>;
+    const [isClicked, setIsClicked] = useState(false);
+
+    const handleClick = () => {
+        setIsClicked(!isClicked);
+    };
+
+    return (
+        <HeartListButton onClick={handleClick} isClicked={isClicked}>
+            찜한 가게
+        </HeartListButton>
+    );
 }
 
-const HeartListButton = styled.button`
+const HeartListButton = styled.button<{ isClicked: boolean }>`
     width: 76px;
     height: 33px;
     border-radius: 8px;
-    background: #fff1ee;
-    color: #ff7455;
+    background: ${(props) => (props.isClicked ? '#fff1ee' : '#d3d3d3')};
+    color: ${(props) => (props.isClicked ? '#FF7455' : '#767676')};
     text-align: center;
-
+    font-family: Pretendard-Regular;
     font-size: 14px;
     font-style: normal;
     font-weight: 600;
