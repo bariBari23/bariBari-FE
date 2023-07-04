5e0bd908df85c2566215a94eb01f10a6bcd551b4
hmuri <113418916+hmuri@users.noreply.github.com>
2023-07-04 14:43:04 +0900

    feat: 픽업 시간, 결제 수단 완성

--- baribari/src/page/Order.tsx
+++ baribari/src/page/Order.tsx
@@ -18,12 +18,35 @@ export default function Order(){
                     <div style={{fontSize: '18px', fontWeight: '700'}}>주문자 정보</div>
                     <PhoneInput placeholder='휴대폰 번호를 입력해주세요'></PhoneInput>
                 </InfoBox>
-                <InfoBox style={{height: '201px', paddingTop: '20px'}}>
-                    <div style={{fontSize: '18px', fontWeight: '700'}}>픽업 시간</div>
-                    <CheckBox></CheckBox>
+                <InfoBox style={{height: '201px', padding: '24px 16px 24px 16px'}}>
+                    <div style={{fontSize: '18px', fontWeight: '700', lineHeight: '28px', marginBottom: '20px'}}>픽업 시간</div>
+                    <div style={{display: 'flex', flexDirection: 'row'}}>
+                        <div style={{width: '147px', display: 'flex', flexDirection: 'column'}}>
+                            <div style={{display: 'flex', paddingBottom: '20px'}}><CheckBox type = 'radio' name='time' value = '8'/><TimeBox>8:00 ~ 9:00</TimeBox></div>
+                            <div style={{display: 'flex', paddingBottom: '20px'}}><CheckBox type = 'radio' name='time' value = '8'/><TimeBox>9:00 ~ 10:00</TimeBox></div>
+                            <div style={{display: 'flex', paddingBottom: '20px'}}><CheckBox type = 'radio' name='time' value = '8'/><TimeBox>10:00 ~ 11:00</TimeBox></div>
+                            <div style={{display: 'flex'}}><CheckBox type = 'radio' name='time' value = '8'/><TimeBox>11:00 ~ 12:00</TimeBox></div>
+                        </div>
+                        <div style={{width: '147px', display: 'flex', alignItems: 'center', flexDirection: 'column'}}>
+                            <div style={{display: 'flex', paddingBottom: '20px'}}><CheckBox type = 'radio' name='time' value = '8'/><TimeBox>12:00 ~ 13:00</TimeBox></div>
+                            <div style={{display: 'flex', paddingBottom: '20px'}}><CheckBox type = 'radio' name='time' value = '8'/><TimeBox>13:00 ~ 14:00</TimeBox></div>
+                            <div style={{display: 'flex'}}><CheckBox type = 'radio' name='time' value = '8'/><TimeBox>14:00 ~ 15:00</TimeBox></div>
+                        </div>
+                    </div>
                 </InfoBox>
-            
-                <ReviewBox isSelected = {active==="리뷰"}/>
+                <InfoBox style={{height: '92px', padding: '24px 16px 24px 16px'}}>
+                <div style={{fontSize: '18px', fontWeight: '700', lineHeight: '28px', marginBottom: '20px'}}>결제 수단</div>
+                <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
+                    <div style={{display: 'flex'}}><CheckBox type = 'radio' name='pay' value = '8'/><TimeBox>카카오페이</TimeBox></div>
+                    <div style={{display: 'flex'}}><CheckBox type = 'radio' name='pay' value = '8'/><TimeBox>무통장 입금</TimeBox></div>
+                    <div style={{display: 'flex'}}><CheckBox type = 'radio' name='pay' value = '8'/><TimeBox>현장 결제</TimeBox></div>
+                </div>
+                </InfoBox>
+                <InfoBox style={{paddingBottom: '30px'}}>
+                    <ListBox>
+
+                    </ListBox>
+                </InfoBox>
                 <AddBtn>19,000원 결제하기</AddBtn>
             </InsideBox>
         </Container>
@@ -48,7 +71,7 @@ const InfoBox = styled.div`
     display: flex;
     height: 92px;
     background-color: white;
-    padding: 26px 16px 20px 8px;
+    padding: 26px 16px 20px 16px;
     margin-bottom: 14px;
     flex-direction: column;
 `
@@ -64,20 +87,46 @@ const PhoneInput = styled.input`
     font-weight: 600;
 `
 const CheckBox = styled.input`
+    appearance: none;
     display: flex;
-    height: 140px;
-    margin-top: 20px;
-    padding-bottom: 1px;
-    border: none;
-    color: #767676;
-    font-size: 16px;
-    font-weight: 600;
+    width: 18px;
+    height: 18px;
+    border-radius: 2px;
+    border: solid 2px #767676;
+    position: relative;
+    padding-right: 12px;
+    
+    &:checked {
+        background-color: #FF7455;
+        border-color: #FF7455;
+    }
+
+    &:checked::before {
+        content: "";
+        position: absolute;
+        margin: auto;
+        border: solid white;
+        left: 4px;
+        top: 1px;
+        border-width: 0 2px 2px 0;
+        height: 6px;
+        width: 4px;
+        transform: rotate(45deg);
+    }
 `
+const TimeBox = styled.div`
+    display: flex;
+    font-size: 16px;
+    font-weight: 500;
+    line-height: 20px;
+    padding-left: 12px;
+`
+
 const AddBtn = styled.div`
     display: flex;
     height: 64px;
     width: calc(100% - 64px);
-    max-width: 432px;
+    max-width: 464px;
     border-radius: 12px;
     background: #FF7455;
     color: #FFF;
