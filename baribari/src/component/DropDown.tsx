1ad36a46ff48fd268ddb9de71aa3a1bd3abeb19d
Yelynn Oh <yoyelynn@gmail.com>
2023-07-04 16:10:59 +0900

    style: DropDown 스타일 완성

--- baribari/src/component/DropDown.tsx
+++ baribari/src/component/DropDown.tsx
@@ -1,6 +1,7 @@
 import { styled } from 'styled-components';
 import { useState } from 'react';
 import { DPointerIcon, UPointerIcon } from './Icon';
+import { ReactComponent as HorizontalLine } from '../assets/horizontalLine2.svg';
 
 export default function DropDown() {
     const [viewOpen, setViewOpen] = useState(false);
@@ -24,8 +25,10 @@ export default function DropDown() {
             {viewOpen && (
                 <DropDownList>
                     <DropDownListItem onClick={() => handleSelectValue('최신순')}>최신순</DropDownListItem>
-                    <DropDownListItem onClick={() => handleSelectValue('가격 낮은 순')}>가격 낮은 순</DropDownListItem>
-                    <DropDownListItem onClick={() => handleSelectValue('가격 높은 순')}>가격 높은 순</DropDownListItem>
+                    <HorizontalLine />
+                    <DropDownListItem onClick={() => handleSelectValue('가격 낮은순')}>가격 낮은순</DropDownListItem>
+                    <HorizontalLine />
+                    <DropDownListItem onClick={() => handleSelectValue('가격 높은순')}>가격 높은순</DropDownListItem>
                 </DropDownList>
             )}
         </DropDownContainer>
@@ -34,16 +37,17 @@ export default function DropDown() {
 
 const DropDownContainer = styled.div`
     position: relative;
-    margin: 20px 0px;
+    margin: 20px 0px 16px 0px;
 `;
 
 // css 나중에 수정하기..
 const DropDownButton = styled.button`
+    margin-left: 16px;
     width: 112px;
-    height: 20px;
     display: flex;
-    padding: 8px 8px 8px 12px;
+    padding: 0 0 0 0;
     align-items: center;
+    justify-content: center;
     border-radius: 8px;
     border: 1px solid #767676;
     background: #f9f9f9;
@@ -53,17 +57,23 @@ const DropDownButton = styled.button`
     font-weight: 600;
     color: #767676;
     line-height: normal;
+    font-family: Pretendard-Regular;
+    gap: 0px;
 `;
 
 const DropDownList = styled.div`
     width: 112px;
+    margin-left: 16px;
+    display: flex;
     position: absolute;
     flex-direction: column;
     justify-content: center;
-    align-items: center;
-    gap: 8px;
+    align-items: flex-start;
+
     border-radius: 8px;
     background: #f9f9f9;
+    font-weight: 600;
+
     box-shadow: 0px 2px 4px 0px rgba(33, 33, 33, 0.15);
 `;
 
