55319d39559d54f7e1a2c2f0d7394c7c879f8252
Yelynn Oh <yoyelynn@gmail.com>
2023-07-31 16:54:49 +0900

    feat: StoreDetailBox에 지도 띄우기

--- baribari/src/component/StoreDetail/StoreDetailBox.tsx
+++ baribari/src/component/StoreDetail/StoreDetailBox.tsx
@@ -2,6 +2,8 @@ import { useQuery } from 'react-query';
 import styled from 'styled-components';
 import { searchById } from '../../apis/api/search';
 import { getStoreInfo } from '../../apis/api/store';
+import MapContainer from '../Map/MapContainer';
+import { useState } from 'react';
 
 export default function StoreDetailBox({ isSelected, storeId }: { isSelected: boolean; storeId: number | null }) {
     const { data: storeData, isLoading, error } = useQuery(['dosirakData', storeId], () => getStoreInfo(storeId), {});
@@ -11,6 +13,11 @@ export default function StoreDetailBox({ isSelected, storeId }: { isSelected: bo
     if (isLoading) {
         return <div>Loading...</div>; //로딩되는 시간 동안 뭐 띄우고 싶으면 사용
     }
+
+    const storeAddress = '서울특별시 마포구 신촌로24길 4';
+    const userPosition = { latitude: 0, longitude: 0 };
+    // const [userPosition, setUserPosition] = useState({ latitude: 0, longitude: 0 });
+
     return (
         <Container isSelected={isSelected}>
             <MainBox>
@@ -60,7 +67,13 @@ export default function StoreDetailBox({ isSelected, storeId }: { isSelected: bo
                         </div>{' '}
                     </div>
                 </InfoBox>
-                <MapBox />
+                <MapContainer
+                    size={['93vw', 180]}
+                    userAddress={storeAddress}
+                    userPosition={userPosition}
+                    isSearched={true}
+                    isStoreLocation={true}
+                />
             </SubBox>
             <SubBox style={{ height: '250px' }}>
                 <InfoBox>
