af68498c2c91f2af2924faf1918e0420ba69346b
hmuri <113418916+hmuri@users.noreply.github.com>
2023-08-04 00:58:37 +0900

    feat: storeDetail 페이지 스타일 수정

--- baribari/src/component/StoreDetail/FoodDetailBox.tsx
+++ baribari/src/component/StoreDetail/FoodDetailBox.tsx
@@ -18,6 +18,7 @@ export default function FoodDetailBox({
 }) {
     const [isFilled, setIsFilled] = useState(dosirakData.data.likedStore);
     const fillColor = isFilled ? '#FF7455' : 'none';
+    const unfillColor = isFilled ? '#FF7455' : '#767676';
     useEffect(() => {
         const fillColor = isFilled ? '#FF7455' : 'none';
     }, [isFilled]);
@@ -75,7 +76,7 @@ export default function FoodDetailBox({
                             >
                                 <div style={{ marginRight: '12px' }}>
                                     {isNaN(parseFloat(storeData.data.reviewMean))
-                                        ? '정보 없음'
+                                        ? '0.0'
                                         : parseFloat(storeData.data.reviewMean).toFixed(1)}
                                 </div>
 
@@ -91,11 +92,11 @@ export default function FoodDetailBox({
                         </div>
                     </StoreNameBox>
                     <div style={{ padding: '3px', marginRight: '0' }} onClick={handleClick}>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 20 22">
                             <path
                                 d="M8.99999 3.24742C8.99999 3.24742 7.99999 0.75 4.50024 0.75C1.5 0.75 0 3.23794 0 5.73536C0 11.565 8.99999 16.6731 8.99999 16.6731C8.99999 16.6731 18 11.565 18 5.73536C18 3.23794 16.5 0.75 13.4997 0.75C9.99999 0.75 8.99999 3.24742 8.99999 3.24742Z"
                                 fill={fillColor}
-                                stroke="#FF7455"
+                                stroke={unfillColor}
                             />
                         </svg>
                     </div>
@@ -163,6 +164,7 @@ const StoreImageBox = styled.img`
     margin-right: 16px;
     background-color: grey;
     border-radius: 8px;
+    object-fit: cover;
 `;
 
 const StoreNameBox = styled.div`
